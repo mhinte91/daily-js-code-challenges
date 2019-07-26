8b4dba0fa8da256c2e3e5c19fb62c60cde30f379
Michael Hinte <michaelhinte@Michaels-MacBook-Pro.local>
2019-07-26 10:51:45 -0500

    Solution 07

--- challenges.js
+++ challenges.js
@@ -210,7 +210,10 @@ Difficulty: Basic
 
 Prompt:
 
-- Write a function called reverseUpcaseString that accepts a single string argument, then returns the string with its characters in reverse orderand converts all characters to uppercase.
+- Write a function called reverseUpcaseString that
+ accepts a single string argument, then returns the 
+ string with its characters in reverse orderand converts 
+ all characters to uppercase.
 
 Examples:
 
@@ -218,7 +221,9 @@ reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES"
 -----------------------------------------------------------------*/
 // Your solution for 07-reverseUpcaseString here:
 
-
+function reverseUpcaseString(string){
+return string.split('').reverse().join('').toUpperCase();
+}
 
 
 
